0cabb9f3213c2508fbbcb8f1f52c12e4f6f0eaba
Muhammad Yousuf <60569307+yousuf550@users.noreply.github.com>
2020-02-23 13:09:07 +0500

    Add files via upload

---
+++ Python Assignment-1.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[28]:
+
+
+FirstName = "Muhammad Yousuf"
+LastName = "Muhammad Hanif"
+DateofBirth = "Feb 07, 1997"
+Education = "Bachelor In Commerce (B.com)"
+Nationality = "Pakistani"
+Religion = "Islam"
+
+print( "First Name: ",FirstName) 
+print( "Last Name: ",LastName) 
+print( "Date of Birth: ",DateofBirth) 
+print( "Eductaion: ",Education) 
+print( "Nationality: ",Nationality) 
+print( "Religion: ",Religion) 
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
